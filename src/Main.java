c491fefffa633bcfa1bd43ec3126bd6cdc133efb
ElijahGoat326 <elijahlopez326@gmail.com>
2023-01-11 09:04:19 -0500

    Added 10 controls

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
 
 public class Main extends Application {
 
@@ -24,6 +24,37 @@ public class Main extends Application {
 
             progressBar.setProgress(0.5);
 
+            ChoiceBox choiceBox = new ChoiceBox();
+
+            choiceBox.getItems().add("Mr beast");
+            choiceBox.getItems().add("Pewdiepie");
+            choiceBox.getItems().add("Walter White");
+
+            ComboBox comboBox = new ComboBox();
+
+            comboBox.getItems().add("Goku");
+            comboBox.getItems().add("Puss and boots");
+            comboBox.getItems().add("Spongebob");
+
+
+            Slider slider = new Slider(0, 100, 0);
+
+
+            ListView listView = new ListView();
+
+            listView.getItems().add("Apples");
+            listView.getItems().add("Pears");
+            listView.getItems().add("bananas");
+
+            DatePicker datePicker = new DatePicker();
+
+            MenuItem menuItem1 = new MenuItem("Spaghetti");
+            MenuItem menuItem2 = new MenuItem("Pizza");
+            MenuItem menuItem3 = new MenuItem("Garlic bread");
+
+            MenuButton menuButton = new MenuButton("Options", null, menuItem1, menuItem2, menuItem3);
+
+
             TextArea textArea = new TextArea();
 
             Label label1 = new Label(" Happy ");
@@ -35,8 +66,8 @@ public class Main extends Application {
             Label label7 = new Label(" controls");
             // Layout the controls
             HBox hbox2 = new HBox(label6,label7);
-            VBox vbox1 = new VBox(label4,label5, colorPicker,progressBar,hbox2,textArea);
-            HBox hbox1 = new HBox(label1, label2,label3, vbox1);
+            VBox vbox1 = new VBox(label4,label5, colorPicker,progressBar,hbox2,textArea,slider);
+            HBox hbox1 = new HBox(label1, label2,label3, vbox1,choiceBox,comboBox,listView,datePicker,menuButton );
             Scene scene = new Scene(hbox1, 400, 200);
             // Put the layout into the scene
             primaryStage.setScene(scene);
